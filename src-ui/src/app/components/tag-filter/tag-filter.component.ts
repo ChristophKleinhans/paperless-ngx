2e729f8a4441e56e862b63d3dc3436014752df2c
christel <christel@kleinhans-web.de>
2025-03-07 21:04:32 +0000

    tags simply placed in the container

--- src-ui/src/app/components/tag-filter/tag-filter.component.ts
+++ src-ui/src/app/components/tag-filter/tag-filter.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { TagComponent } from '../common/tag/tag.component'
+import { TagService } from 'src/app/services/rest/tag.service'
+import { Tag } from 'src/app/data/tag'
 
 @Component({
     selector: 'pngx-tag-filter',
     templateUrl: './tag-filter.component.html',
     styleUrls: ['./tag-filter.component.scss'],
     standalone: true,
-    imports: [CommonModule]
+    imports: [CommonModule, TagComponent]
 })
-export class TagFilterComponent {
-    constructor() { }
+export class TagFilterComponent implements OnInit {
+    tags: Tag[] = []
+
+    constructor(private tagService: TagService) { }
+
+    ngOnInit() {
+        this.tagService.listAll().subscribe(response => {
+            this.tags = response.results
+        })
+    }
 }
