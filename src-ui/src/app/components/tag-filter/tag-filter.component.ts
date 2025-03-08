08e62c3db3ee341df5613329c3cd2410713ba621
christel <christel@kleinhans-web.de>
2025-03-08 15:52:29 +0000

    Reload the tag-filter container on each create/update/delete of  a tag; having a spinner when tag container is loading

--- src-ui/src/app/components/tag-filter/tag-filter.component.ts
+++ src-ui/src/app/components/tag-filter/tag-filter.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TagComponent } from '../common/tag/tag.component'
 import { TagService } from 'src/app/services/rest/tag.service'
 import { Tag } from 'src/app/data/tag'
+import { Subject } from 'rxjs'
+import { takeUntil, tap } from 'rxjs/operators'
 
 @Component({
     selector: 'pngx-tag-filter',
@@ -11,14 +13,48 @@ import { Tag } from 'src/app/data/tag'
     standalone: true,
     imports: [CommonModule, TagComponent]
 })
-export class TagFilterComponent implements OnInit {
+export class TagFilterComponent implements OnInit, OnDestroy {
     tags: Tag[] = []
+    private unsubscribeNotifier = new Subject()
+    loading = false
+    show = false
 
-    constructor(private tagService: TagService) { }
+    constructor(private tagService: TagService) {
+        // Store original methods
+        const originalCreate = this.tagService.create.bind(this.tagService)
+        const originalUpdate = this.tagService.update.bind(this.tagService)
+        const originalDelete = this.tagService.delete.bind(this.tagService)
+
+        // Override methods to add reload after each operation
+        this.tagService.create = (tag: Tag) => originalCreate(tag).pipe(tap(() => this.loadTags()))
+        this.tagService.update = (tag: Tag) => originalUpdate(tag).pipe(tap(() => this.loadTags()))
+        this.tagService.delete = (tag: Tag) => originalDelete(tag).pipe(tap(() => this.loadTags()))
+    }
 
     ngOnInit() {
-        this.tagService.listAll().subscribe(response => {
-            this.tags = response.results
+        this.loadTags()
+    }
+
+    ngOnDestroy() {
+        this.unsubscribeNotifier.next(null)
+        this.unsubscribeNotifier.complete()
+    }
+
+    private loadTags() {
+        this.loading = true
+        this.show = false
+        this.tagService.listAll().pipe(
+            takeUntil(this.unsubscribeNotifier)
+        ).subscribe({
+            next: response => {
+                this.tags = response.results
+                this.show = true
+                this.loading = false
+            },
+            error: err => {
+                console.error('Error loading tags:', err)
+                this.loading = false
+            }
         })
     }
 }
